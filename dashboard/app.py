b865625acb4d791c2dec8ec1d358a2a2034bb42b
PedroH <pedrox90w@gmail.com>
2025-08-22 18:37:41 -0300

    end of the day

--- dashboard/app.py
+++ dashboard/app.py
@@ -3,7 +3,11 @@ from src.charts.line_charts import grafico_linhas
 from src.queries.queries import time_vs_temp, temp_vs_speed, time_vs_power, temp_vs_power
 
 # Título da aplicação
-st.title("Meu Processador")
+st.markdown(
+    "<h1 style='text-align: center; color: black;'>Meu Processador</h1>",
+    unsafe_allow_html=True
+)
+
 
 df_temp_vs_speed = temp_vs_speed()
 grafico = grafico_linhas(df_temp_vs_speed, "core temp", "core speed", 'type', titulo='Temperatura do Núcleo vs Velocidade do Núcleo')
@@ -18,5 +22,5 @@ grafico = grafico_linhas(df_time_vs_power, "time of day", "cpu power", 'type', t
 st.altair_chart(grafico, use_container_width=True)
 
 df_temp_vs_power = temp_vs_power()
-grafico = grafico_linhas(df_temp_vs_power, "core temp", "cpu power", 'type', titulo='Energia do CPU em Função da Temperatura do Núcleo')
+grafico = grafico_linhas(df_temp_vs_power, "core temp", "cpu power", 'type', titulo='Energia do CPU vs Temperatura do Núcleo')
 st.altair_chart(grafico, use_container_width=True)
